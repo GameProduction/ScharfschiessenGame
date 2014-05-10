a892d516a3ddeeedb1513bacce92098163882974
Linda Schey <lindaschey@t-online.de>
2014-05-10 17:17:20 +0200

    löschen falsches Projekt

--- Fusee-Scharfschiessen/src/Engine/Common/IShaderProgramImp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fusee.Engine
-{
-    /// <summary>
-    /// The Shaderprogram interface. It is currently in prototype state.
-    /// </summary>
-    public interface IShaderProgramImp
-    {
-    }
-}
