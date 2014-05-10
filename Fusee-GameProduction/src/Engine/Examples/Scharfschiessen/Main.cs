b922eb41c99ea1d8587b93053dfada20f899d189
Linda Schey <lindaschey@t-online.de>
2014-05-10 17:27:12 +0200

    so jetz aber das richtige Projekt

---
+++ Fusee-GameProduction/src/Engine/Examples/Scharfschiessen/Main.cs
@@ -0,0 +1,51 @@
+using System.Diagnostics;
+using System.Windows.Forms;
+using Fusee.Engine;
+using Fusee.SceneManagement;
+using Fusee.Math;
+using System.Windows;
+
+
+namespace Examples.Scharfschiessen
+{
+    public class Scharfschiessen : RenderCanvas
+    {
+        // is called on startup
+        public override void Init()
+        {
+            RC.ClearColor = new float4(0.1f, 0.1f, 0.5f, 1);
+            SetWindowSettings();
+        }
+
+        internal void SetWindowSettings()
+        {
+            //Höhe und Breite an Primären Bildschirm anpassen
+            Width = Screen.PrimaryScreen.Bounds.Width;
+            Height = Screen.PrimaryScreen.Bounds.Height/6;
+            Debug.Write(Width);
+        }
+
+        // is called once a frame
+        public override void RenderAFrame()
+        {
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            Present();
+        }
+
+        // is called when the window was resized
+        public override void Resize()
+        {
+            RC.Viewport(0, 0, Width, Height);
+
+            var aspectRatio = Width / (float)Height;
+            RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10000);
+        }
+
+        public static void Main()
+        {
+            var app = new Scharfschiessen();
+            app.Run();
+        }
+    }
+}
