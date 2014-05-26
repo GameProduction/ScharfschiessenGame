c69fd2f93b66c38891d20420edd70a3e6453aaa2
Sunny-Day <susanne.schmidt@hs-furtwangen.de>
2014-05-26 13:22:01 +0200

    Aktueller Stand GUI

--- Fusee-GameProduction/src/Engine/Examples/Scharfschiessen/Gui.cs
+++ Fusee-GameProduction/src/Engine/Examples/Scharfschiessen/Gui.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 using Fusee.Engine;
 using Fusee.Math;
 
@@ -17,7 +18,6 @@ namespace Examples.Scharfschiessen
         private IFont _guiFontCabin12;
         private IFont _guiFontCabin18;
         private IFont _guiFontCabin24;
-        private GUIText _guiText;
         private GUIText _guiText1;
         private GUIText _guiText2;
         private GUIButton[] _guiDiffs;
@@ -34,10 +34,16 @@ namespace Examples.Scharfschiessen
             _guiFontCabin18 = RC.LoadFont("Assets/Cabin.ttf", 18);
             _guiFontCabin24 = RC.LoadFont("Assets/Cabin.ttf", 24);
 
+            var height = Screen.PrimaryScreen.Bounds.Width;
+            var width = Screen.PrimaryScreen.Bounds.Height;
+
             _guiDiffs = new GUIButton[3];
-            _guiDiffs[0] = new GUIButton(0, 0, 40, 20);
+            _guiDiffs[0] = new GUIButton(width, height/15, 40, 20);
             _guiDiffs[1] = new GUIButton(40, 0, 40, 20);
-            _guiDiffs[2] = new GUIButton(610, 70, 100, 20);
+            _guiDiffs[2] = new GUIButton(0, 0, 40, 20);
+
+            Console.WriteLine("Höhe = " + height);
+            Console.WriteLine("Breite = " + width);
 
         }
 
@@ -85,14 +91,16 @@ namespace Examples.Scharfschiessen
             _guiText2.TextColor = new float4(0, 0, 0, 1);
             _mainmenuHandler.Add(_guiText1);
             _mainmenuHandler.Add(_guiText2);
-            _guiHandler.Add(_guiDiffs[2]);
-           
+            _guiHandler.Add(_guiDiffs[0]);
+            _guiDiffs[0].OnGUIButtonDown += OnDiffButtonDown;
+
         }
 
         internal void InGameGui()
         {
             //set guiHander für während das Spiel läuft (während der Pause?)
             Console.WriteLine("InGameGui");
+            _guiDiffs[0].OnGUIButtonDown -= OnDiffButtonDown;
             _guiHandler = _inGameHandler;
             _guiText1 = new GUIText("Zeit:", _guiFontCabin12, 10, 15);
             _guiText1.TextColor = new float4(0, 0, 0, 1);
@@ -112,7 +120,15 @@ namespace Examples.Scharfschiessen
             _guiText2.TextColor = new float4(0, 0, 0, 1);
             _highScoreHandler.Add(_guiText1);
             _highScoreHandler.Add(_guiText2);
-            _guiHandler.Add(_guiDiffs[2]);
+            _guiHandler.Add(_guiDiffs[0]);
+            _guiDiffs[0].OnGUIButtonDown += OnDiffButtonDown;
+
         }
+
+        private void OnDiffButtonDown(GUIButton sender, Fusee.Engine.MouseEventArgs mea)
+        {
+        InGameGui();
+        }
+
     }
 }
