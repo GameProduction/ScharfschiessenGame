1e46ed73f8b0ccdda3549a16f2d1c24db2a6099e
2of3 <lindaschey@t-online.de>
2014-06-01 12:26:56 +0200

    aufgräumt

--- Fusee-GameProduction/src/Engine/Examples/Scharfschiessen/Main.cs
+++ Fusee-GameProduction/src/Engine/Examples/Scharfschiessen/Main.cs
@@ -12,39 +12,15 @@ namespace Examples.Scharfschiessen
 {
     public class Scharfschiessen : RenderCanvas
     {
-        internal Mesh Mesh;
-        internal Mesh MeshCube;
         
         public GameHandler GameHandler;
-       // internal GameState GameState;
-        //internal Gui Gui;
-
-        private float rot;
-        // variables for shader
-        private ShaderProgram _spColor;
-        private IShaderParam _colorParam;
       
-
-        private static float _angleHorz, _angleVert, _angleVelHorz, _angleVelVert;
-        private float4x4 rotY = float4x4.Identity;
-        private float4x4 rotX = float4x4.Identity;
-        private const float RotationSpeed = 0.71f;
-        private const float Damping = 0.75f;
-
-
-        // is called on startup
+      // is called on startup
         public override void Init()
         {
             SetWindowSize(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height / 5, true, 0, 0);
-            
-            rot = 0;
+           
             RC.ClearColor = new float4(0.9f, 0.9f, 0.9f, 1);
-
-            Mesh = MeshReader.LoadMesh(@"Assets/Teapot.obj.model");
-            MeshCube = MeshReader.LoadMesh(@"Assets/Cube.obj.model");
-            _spColor = MoreShaders.GetDiffuseColorShader(RC);
-            _colorParam = _spColor.GetShaderParam("color");
-
 
             GameHandler = new GameHandler(RC, this);
 
@@ -59,28 +35,6 @@ namespace Examples.Scharfschiessen
 
             GameHandler.Update();
           
-
-            /*RC.SetShader(_spColor);
-
-            // first mesh
-            // Row order notation
-            var modelViewMesh1 = mtxCam * float4x4.CreateTranslation(0, 10, 200)* float4x4.CreateRotationY(45) * float4x4.Scale(0.1f);
-            RC.ModelView = modelViewMesh1;
-            RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
-
-            RC.Render(MeshCube);
-
-            rot += (float)Time.Instance.DeltaTime * 5;
-
-            var modelView = mtxCam * mtxRot * float4x4.CreateTranslation(-100, 10, 200) * float4x4.CreateRotationX(rot) * float4x4.Scale(0.25f);
-            RC.SetShader(_spColor);
-            RC.ModelView = modelView;
-            RC.SetShaderParam(_colorParam, new float4(0.5f, 0.8f, 0, 1));
-            RC.Render(Mesh);
-
-       
-            TestInput();*/
-
             Present();
         }
 
