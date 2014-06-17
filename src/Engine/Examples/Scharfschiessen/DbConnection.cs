8f904c1adad6865c12f5eb8c8ac0d77ee4e58ed6
ramogundogdu <ramazan.guendogdu@hs.furtwangen.de>
2014-06-17 16:14:08 +0200

    DB Connection Testing
    Connection created and inserted a String

--- src/Engine/Examples/Scharfschiessen/DbConnection.cs
+++ src/Engine/Examples/Scharfschiessen/DbConnection.cs
@@ -1,29 +1,217 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
 
 namespace Examples.Scharfschiessen
 {
     class DbConnection
     {
-        protected void CreateDbConnection(object sender, EventArgs e)
-        {
-            SqlConnection con = new SqlConnection("Server=RAMO-PC; Database=Schafschiessen; Integreted Security=SSPI;");
-            con.Open();
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
 
-            //SQL-Befehle erstellen
-            SqlCommand cmd = new SqlCommand("Select Name from Points", con);
-            SqlDataReader dr = cmd.ExecuteReader();
-            while (dr.Read())
-            {
-                //Spaltenwerte aus der Tabelle lesen
-                Console.Out.WriteLine(dr["Name"].ToString() + "<br>");
-            }
-            con.Close();
-            con.Dispose();
+        // Construktor
+        public DbConnection()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "87.106.27.181:3306";
+            database = "schafschiessenDB";
+            uid = "db_anwendung";
+            password = "FEKnnqERy7MHEDpr";
+
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
 
         }
+
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("Kann keine Verbindung zum Server erstellt werden. Kontaktieren Sie bitte den Administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Benutzername/Kennwort ungültig, bitte wiederholen!");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert()
+        {
+            string query = "INSERT INTO PlayerPoints (PlayerName, HighScore) Values( 'Ramo', 10000)";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update()
+        {
+            //string query = "UPDATE PlayerPoints SET PlayerName='ThePlayer', HighScore='999' WHERE PlayerName='Ramo'";
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+
+                //Assign the query using CommandText
+                //cmd.CommandText = query;
+
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+            //string query = "DELETE FROM PlayerPoints WHERE PlayerName='Ramo'";
+
+            if (this.OpenConnection() == true)
+            {
+                //MySqlCommand cmd = new MySqlCommand(query, connection);
+                //cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        //Select statement
+        public List<string>[] Select()
+        {
+            string query = "SELECT * FROM PlayerPoints";
+
+            //Create a list to store the result
+            List<string>[] list = new List<string>[2];
+            list[0] = new List<string>();
+            list[1] = new List<string>();
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list
+                while (dataReader.Read())
+                {
+                    list[1].Add(dataReader["PlayerName"] + "");
+                    list[2].Add(dataReader["HighScore"] + "");
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                this.CloseConnection();
+
+                //return list to be displayed
+                return list;
+            }
+            else
+            {
+                return list;
+            }
+        }
+
+        //Count statement
+        public int Count()
+        {
+            string query = "SELECT Count(*) FROM PlayerPoints";
+            int Count = -1;
+
+            //Open Connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Mysql Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //ExecuteScalar will return one value
+                Count = int.Parse(cmd.ExecuteScalar() + "");
+
+                //close Connection
+                this.CloseConnection();
+
+                return Count;
+            }
+            else
+            {
+                return Count;
+            }
+        }
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
+        {
+        }
+
+
+
     }
 }
